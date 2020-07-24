fb4ce745119a65b84cd0749cadb9a6798b064292
Marcos Alves <marcosrda@MarckRDA.localdomain>
2020-07-24 17:39:53 -0300

    First commit

---
+++ CalcIdealWeight.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace IdealWeight
+{
+    partial class CalcIdealWeight
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbGender = new System.Windows.Forms.GroupBox();
+            this.rbFemale = new System.Windows.Forms.RadioButton();
+            this.rbMale = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbIdealWeight = new System.Windows.Forms.Label();
+            this.gbGender.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbGender
+            // 
+            this.gbGender.Controls.Add(this.rbFemale);
+            this.gbGender.Controls.Add(this.rbMale);
+            this.gbGender.Location = new System.Drawing.Point(12, 24);
+            this.gbGender.Name = "gbGender";
+            this.gbGender.Size = new System.Drawing.Size(200, 62);
+            this.gbGender.TabIndex = 0;
+            this.gbGender.TabStop = false;
+            this.gbGender.Text = "Gender : ";
+            // 
+            // rbFemale
+            // 
+            this.rbFemale.AutoSize = true;
+            this.rbFemale.Location = new System.Drawing.Point(99, 37);
+            this.rbFemale.Name = "rbFemale";
+            this.rbFemale.Size = new System.Drawing.Size(59, 17);
+            this.rbFemale.TabIndex = 1;
+            this.rbFemale.TabStop = true;
+            this.rbFemale.Text = "Female";
+            this.rbFemale.UseVisualStyleBackColor = true;
+            this.rbFemale.CheckedChanged += new System.EventHandler(this.rbMale_CheckedChanged);
+            // 
+            // rbMale
+            // 
+            this.rbMale.AutoSize = true;
+            this.rbMale.Location = new System.Drawing.Point(17, 37);
+            this.rbMale.Name = "rbMale";
+            this.rbMale.Size = new System.Drawing.Size(48, 17);
+            this.rbMale.TabIndex = 0;
+            this.rbMale.TabStop = true;
+            this.rbMale.Text = "Male";
+            this.rbMale.UseVisualStyleBackColor = true;
+            this.rbMale.CheckedChanged += new System.EventHandler(this.rbMale_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(265, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Height (cm) :";
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(268, 58);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 20);
+            this.txtHeight.TabIndex = 2;
+            this.txtHeight.TextChanged += new System.EventHandler(this.txtHeight_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(162, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ideal Weight";
+            // 
+            // lbIdealWeight
+            // 
+            this.lbIdealWeight.AutoSize = true;
+            this.lbIdealWeight.Font = new System.Drawing.Font("MS PGothic", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbIdealWeight.ForeColor = System.Drawing.Color.Red;
+            this.lbIdealWeight.Location = new System.Drawing.Point(151, 131);
+            this.lbIdealWeight.Name = "lbIdealWeight";
+            this.lbIdealWeight.Size = new System.Drawing.Size(0, 29);
+            this.lbIdealWeight.TabIndex = 4;
+            // 
+            // CalcIdealWeight
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 169);
+            this.Controls.Add(this.lbIdealWeight);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gbGender);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "CalcIdealWeight";
+            this.Text = "Ideal Weight";
+            this.gbGender.ResumeLayout(false);
+            this.gbGender.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbGender;
+        private System.Windows.Forms.RadioButton rbFemale;
+        private System.Windows.Forms.RadioButton rbMale;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbIdealWeight;
+    }
+}
+
---
+++ CalcIdealWeight.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IdealWeight
+{
+    public partial class CalcIdealWeight : Form
+    {
+        RadioButton rbnSelected = null;
+        public CalcIdealWeight()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void rbMale_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton rb = (RadioButton)sender;
+            if (rb.Checked)
+            {
+                rbnSelected = rb;
+                SetIdealWeight();
+            }
+
+        }
+
+        private void SetIdealWeight()
+        {
+            try
+            {
+                double height = Convert.ToDouble(txtHeight.Text);
+                double idealWeight;
+
+                if (rbnSelected.Text.Equals("Male"))
+                    idealWeight = (height - 100) * 0.9;
+                else
+                    idealWeight = (height - 100) * 0.85;
+                lbIdealWeight.Text = idealWeight.ToString("N");
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("Select the gender and insert the height correctly", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            
+        }
+
+        
+
+        private void txtHeight_TextChanged(object sender, EventArgs e)
+        {
+            if(txtHeight.Text != String.Empty)
+            {
+                SetIdealWeight();
+            }
+               
+        }
+
+        
+    }
+}
